f7207889712d95fbe03e6972576336ba9ef9d4b3
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-10-02 22:16:30 +0300

    updata

--- app.py
+++ app.py
@@ -3,14 +3,22 @@ import time
 
 pygame.init()
 clock = pygame.time.Clock()
+font = pygame.font.Font("fonts/Roboto-Regular.ttf", 15)
 
-font = pygame.font.Font("fonts/Roboto-Regular.ttf", 24)
-text = font.render('123456789', False, 'White')
-button = pygame.Surface((50, 20))
-text_rect = text.get_rect(center=(button.get_width() /2,
-            button.get_height()/2))
+text_start = font.render('start', False, 'White')
+button_start = pygame.Surface((40, 15))
+text_rect_start = text_start.get_rect(center=(button_start.get_width() /2,
+            button_start.get_height()/2))
+button_start_rect = pygame.Rect(192 + 85, 29 + 30, 40, 15)
 
-pygame.init()
+
+
+text_stop = font.render('stop', False, 'White')
+button_stop = pygame.Surface((40, 15))
+text_rect_stop = text_stop.get_rect(center=(button_stop.get_width() /2,
+            button_stop.get_height()/2))
+button_stop_rect = pygame.Rect(192 + 130, 29 + 30, 40, 15)
+
 screen = pygame.display.set_mode((640, 360))
 pygame.display.set_caption("washing machine")
 icon = pygame.image.load('img/washing-machine.jpg')
@@ -21,20 +29,35 @@ washing_machine = pygame.image.load('img/washing-machine.jpg')
 clothing = pygame.image.load('img/clothing.png')
 
 
-
+clock.tick(60)
 running = True
+num_rotate=0
 while running:
     screen.blit(bg, (0, 0))
     screen.blit(washing_machine, (192, 29))
 
-    clothing = pygame.transform.rotate(clothing, -90)
+
     washing_machine.blit(clothing, (77, 100))
-    washing_machine.blit(button, (0, 0))
-    button.blit(text, text_rect)
+
+    washing_machine.blit(button_start, (85, 30))
+    button_start.blit(text_start, text_rect_start)
+
+    washing_machine.blit(button_stop, (130, 30))
+    button_stop.blit(text_stop, text_rect_stop)
+
+
 
     pygame.display.update()
-    clock.tick(10)
 
+    if num_rotate > 0 and working == True:
+        if num_rotate < 120:
+            clock.tick(60)
+            print(int(num_rotate / 60))
+        else:
+            clock.tick(5)
+            print(((num_rotate-120)/5)+(120/60))
+        clothing = pygame.transform.rotate(clothing, -90)
+        num_rotate -= 1
 
 
 
@@ -42,4 +65,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_start_rect.collidepoint(pygame.mouse.get_pos()):
+                num_rotate = 200
+                working = True
+            elif button_stop_rect.collidepoint(pygame.mouse.get_pos()):
+                print(False)
+                working = False
 
